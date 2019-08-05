8cf7a3e681c6d455ed378179a7abf3b36f95664c
SKADaddy <692877906@qq.com>
2019-08-05 20:37:03 +0800

    todo

---
+++ src-todos-part1/components/TodoHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="todo-header">
+    <input type="text" placeholder="请输入你的任务名称，按回车键确认" @keyup.enter="addItem"
+           v-model="title">
+  </div>
+</template>
+
+<script>
+    export default {
+      data () {
+        return {
+          title: ''
+        }
+      },
+      props: {
+        addTodo: {
+          type: Function
+        }
+      },
+      methods: {
+        addItem () {
+          //1、检查输入合法性
+          const title = this.title.trim()
+          if(!title){
+            alert('文本框输入的内容不能为空')
+            return
+          }
+          //2、根据输入生成一个todo对象
+          const todo = {
+            title,
+            complete: false
+          }
+          //3、添加到todos
+          this.addTodo(todo)
+          //4、清除输入框内容
+          this.title=''
+        }
+      }
+    }
+</script>
+
+<style>
+  .todo-header input {
+    width: 560px;
+    height: 28px;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 4px 7px;
+  }
+  .todo-header input:focus {
+    outline: none;
+    border-color: rgba(82, 168, 236, 0.8);
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.75), 0 0 8px rgba(82, 168, 236, 0.5);
+  }
+</style>
---
+++ src-todos-part1/components/TodoItem.vue
@@ -0,0 +1,84 @@
+<template>
+  <!--
+
+  -->
+  <li @mouseenter="handleEnter(true)" @mouseleave="handleEnter(false)" :style="{background: bgColor}">
+    <label>
+      <input type="checkbox" v-model="todo.complete">
+      <span>{{todo.title}}</span>
+    </label>
+    <button class="btn btn-danger" v-show="isShow" @click="deleteItem">删除</button>
+  </li>
+</template>
+
+<script>
+    export default {
+      data () {
+        return {
+          isShow: false, //默认不显示按钮
+          bgColor: 'white' //背景颜色默认
+        }
+      },
+      props: {
+        todo:{
+          type: Object
+        },
+        index: {
+          type: Number
+        },
+        deleteTodo: {
+          type: Function
+        }
+      },
+      methods: {
+        handleEnter (isEnter) {
+          //console.log(isEnter)
+          if(isEnter){
+            this.isShow=true
+            this.bgColor='#aaaaaa'
+          }else{
+            this.isShow=false
+            this.bgColor='white'
+          }
+        },
+        deleteItem (index) {
+          const {todo} = this
+          if(window.confirm('确认删除${todo.title}吗')){
+            this.deleteTodo(index)
+          }
+        }
+      }
+    }
+</script>
+
+<style>
+  /*item*/
+  li {
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #ddd;
+  }
+  li label {
+    float: left;
+    cursor: pointer;
+  }
+  li label li input {
+    vertical-align: middle;
+    margin-right: 6px;
+    position: relative;
+    top: -1px;
+  }
+  li button {
+    float: right;
+    display: none;
+    margin-top: 3px;
+  }
+  li:before {
+    content: initial;
+  }
+  li:last-child {
+    border-bottom: none;
+  }
+</style>
