cd0ba40728bd49757ec966b9ae0584770d32369a
SKADaddy <692877906@qq.com>
2019-08-07 11:05:47 +0800

    入口js

---
+++ src-search_GithubUser/main.js
@@ -0,0 +1,19 @@
+/*
+  入口js：创建vue实例
+ */
+import Vue from 'vue'
+// import VueResource from 'vue-resource'
+import App from './App.vue'
+
+// 声明使用插件
+// Vue.use(VueResource) //内部会给vm对象和组件对象添加一个属性 名字叫$http(get,post)两个方法
+
+// import './base.css'
+
+new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App/>'
+})
